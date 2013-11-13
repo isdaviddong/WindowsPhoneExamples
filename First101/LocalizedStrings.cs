d05c15cf2a4331d66244cc5e4778d762850c8873
isdaviddong <isdaviddong@gmail.com>
2013-11-13 15:24:42 +0800

    v1

---
+++ First101/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using First101.Resources;
+
+namespace First101
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
