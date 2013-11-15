5440fcf4a35359a1e5f0bf28637f733c589ac4d0
isdaviddong <isdaviddong@gmail.com>
2013-11-15 14:30:09 +0800

    加入ApplicationBar

---
+++ TestApplicationBar/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TestApplicationBar.Resources;
+
+namespace TestApplicationBar
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
